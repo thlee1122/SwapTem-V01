aa5be68c49844827e053091173701edcc94bcb4f
Lee <taelee@LEE169736TAL.fios-router.home>
2019-05-20 22:51:04 -0400

    Still testing

--- src/native/components/Profile.js
+++ src/native/components/Profile.js
@@ -145,7 +145,47 @@ class Profile extends React.Component {
               Verifications
             </Text>
 
-            
+            <View style={{flexDirection: 'row', marginLeft: 16, marginRight: 16, marginBottom: 20, display: 'flex'}}>
+              <TouchableOpacity style={{flexDirection: 'column', flex: 1}}>
+                <View style={{backgroundColor: 'black', width: 64, height: 64, borderRadius: 50, alignSelf: 'center'}}>
+                  <FeatherIcon name="phone" size={25} color="white" style={{alignSelf: 'center', marginTop: 18}}/>
+                </View>
+
+                <Text style={{fontSize: 14, lineHeight: 20, textAlign: 'center', flex: 1, marginTop: 16}}>
+                  Verified
+                </Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={{flexDirection: 'column', flex: 1}}>
+                <View style={{backgroundColor: 'black', width: 64, height: 64, borderRadius: 50, alignSelf: 'center'}}>
+                  <FeatherIcon name="mail" size={25} color="white" style={{alignSelf: 'center', marginTop: 18}}/>
+                </View>
+
+                <Text style={{fontSize: 14, lineHeight: 20, textAlign: 'center', flex: 1, marginTop: 16}}>
+                  Verified
+                </Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={{flexDirection: 'column', flex: 1}}>
+                <View 
+                  style={{
+                    backgroundColor: 'white', 
+                    width: 64, 
+                    height: 64, 
+                    borderRadius: 50,
+                    borderColor: 'black',
+                    borderWidth: 1, 
+                    alignSelf: 'center'
+                  }}
+                >
+                  <FeatherIcon name="facebook" size={25} color="black" style={{alignSelf: 'center', marginTop: 18}}/>
+                </View>
+
+                <Text style={{fontSize: 14, lineHeight: 20, textAlign: 'center', flex: 1, marginTop: 16}}>
+                  Not verified
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
         
